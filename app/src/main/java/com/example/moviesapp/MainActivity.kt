1c0d2d331b83fcbffb3606f70aef9109ad7091ea
Dawid Woda <dawid.woda@performgroup.com>
2019-05-29 12:00:25 +0200

    update

--- app/src/main/java/com/example/moviesapp/MainActivity.kt
+++ app/src/main/java/com/example/moviesapp/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.moviesapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.View
+import com.example.moviesapp.api.movies.models.Movie
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
@@ -11,6 +14,8 @@ class MainActivity : AppCompatActivity(), MainActivityContract.View {
     @Inject
     lateinit var presenter: MainActivityPresenter
 
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,9 +23,15 @@ class MainActivity : AppCompatActivity(), MainActivityContract.View {
         presenter.attachView(this)
     }
 
-    override fun setTitle(title: String) {
-        movieTitle.text = title
-        movieTitleProgressBar.visibility = View.GONE
-        movieTitle.visibility = View.VISIBLE
+    override fun setTitle(movieList: List<Movie>) {
+        progressBar.visibility = View.GONE
+
+        viewManager = LinearLayoutManager(this)
+
+        findViewById<RecyclerView>(R.id.movies_recycler_view).apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = MoviesAdapter(movieList)
+        }
     }
 }
--- app/src/main/java/com/example/moviesapp/api/movies/models/Movie.kt
+++ app/src/main/java/com/example/moviesapp/api/movies/models/Movie.kt
@@ -4,5 +4,7 @@ import com.google.gson.annotations.SerializedName
 
 data class Movie(
     @SerializedName("title")
-    val title: String
+    val title: String,
+    @SerializedName("vote_average")
+    val rating: String
 )
