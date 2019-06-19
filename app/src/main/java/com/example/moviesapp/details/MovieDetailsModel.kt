908e09e6397e2e083f4950c5c7d8383c972ca113
Dawid Woda <dawid.woda@performgroup.com>
2019-06-19 09:38:54 +0200

    update

--- app/src/main/java/com/example/moviesapp/details/MovieDetailsModel.kt
+++ app/src/main/java/com/example/moviesapp/details/MovieDetailsModel.kt
@@ -1,17 +1,11 @@
 package com.example.moviesapp.details
 
-import com.example.moviesapp.api.configuration.ConfigurationApi
-import com.example.moviesapp.api.movies.MoviesApi
 import com.example.moviesapp.api.movies.MoviesApiClient
-import com.example.moviesapp.api.movies.models.DiscoverMovies
-import com.example.moviesapp.api.movies.models.MovieDetails
-import io.reactivex.SingleObserver
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
-class MovieDetailsModel @Inject constructor(private val moviesApiClient: MoviesApiClient, private val configurationApi: ConfigurationApi) : MovieDetailsContract.Model {
+class MovieDetailsModel @Inject constructor(private val moviesApiClient: MoviesApiClient) : MovieDetailsContract.Model {
     override fun getMovieDetails(id: Int, onFinishedListener: MovieDetailsContract.Model.OnFinishedListener) {
         moviesApiClient
             .getMovieDetails(id)
